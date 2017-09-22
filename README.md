dbdc5b731a68b9160117f08de881197ade778d0c
David <dcalifornia@Davids-MacBook-Pro.local>
2017-09-21 19:44:23 -0500

    Full functionality: delete, load

---
+++ README.md
@@ -0,0 +1,3 @@
+`hashCharPointers.cpp` is written without using the C++ string type, which is primitive and more C-type than C++-type.
+`hashModern.cpp` uses C++ strings more correctly
+
--- hashCharPointers.cpp
+++ hashCharPointers.cpp
@@ -4,7 +4,12 @@
 #include<string.h>
 //using namespace std;
 
-// TODO char* is bullshit maybe
+// API:
+// constructor DavidHash(int size)
+// boolean set(key, value)
+// get(key)
+// delete(key)
+// float()
 class DavidHash {
     private:
         int size;
@@ -15,35 +20,52 @@ class DavidHash {
             for (int i = 0; i < k_size; i++) {
                 ssf +=  (int)k[i];
                 ssf %= size;
-                printf("%d\n", ssf);
+                printf("Hashing integer: ssf is now %d\n", ssf);
             }
             return ssf;
         }
     public:
         DavidHash(int s) {
-            printf("hey im here\n");
             head = (char**) malloc(sizeof(int*) * s);
             memset(head, 0, sizeof(int*) * s);
             size = s;
             elems = 0;
         }
-        void set(char* k, int k_size, char* v, bool debug=false) {
+        bool set(char* k, int k_size, char* v, bool debug=false) {
             int index = hash(k, k_size);
+            if(head[index] != NULL) {
+                fprintf(stderr, "Conflict! New key: %s, Old key: %s, Index: %d", k, head[index], index);
+                return false;
+            }
             if(debug) {
-                printf("Setting key %s, indexed to %d, to store value %s", k, index, v);
+                printf("Setting key %s, indexed to %d, to store value %s\n", k, index, v);
             }
             head[index] = v;
+            elems++;
+            return true;
         }
         char* get(char* k, int k_size, bool debug=false) {
             int index = hash(k, k_size);
             if(debug) {
-                printf("Getting key %s as indexed to %d", k, index);
+                printf("Getting key %s as indexed to %d\n", k, index);
             }
             return head[index];
+        }
+        void del(char* k, int k_size, bool debug=false) {
+            int index = hash(k, k_size);
+            if(debug) {
+                printf("Deleting value for key %s indexed to %d\n", k, index);
+            }
+            head[index] = NULL;
+            elems--;
+        }
+        float load() {
+            return ((float)elems)/((float)size);
         }
 };
 
 void unitTest(int size) {
+    printf("Unit test for size %d\n", size);
     DavidHash hash_table = DavidHash(size);
 
     int key_len = 4;
@@ -57,12 +79,17 @@ void unitTest(int size) {
     sprintf(value, "what's good?");
  
     hash_table.set(key, key_len-1, value, true);
-    printf("%s\n", hash_table.get(key, key_len-1, true));
+
+    printf("Having called hash_table.set(), hash_table.get() returns %s\n", hash_table.get(key, key_len-1, true));
+    hash_table.del(key, key_len-1, true);
+    
+    printf("After deleting, hash_table.get() returns %s\n", hash_table.get(key, key_len-1, true));
 }
 
 int main() {
     unitTest(10);
     unitTest(100);
+    unitTest(217);
     unitTest(1000);
     return 0;
 }
